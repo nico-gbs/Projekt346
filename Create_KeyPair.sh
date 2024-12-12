17cedf115559b469fc6e627612903f9f94f38fb8
Leandro Rutz <56978556+LNDRRTZ@users.noreply.github.com>
2024-12-12 10:12:06 +0100

    Update and rename initscript.sh to Create_KeyPair.sh
    added Variables + text for understanding

--- Create_KeyPair.sh
+++ Create_KeyPair.sh
@@ -1,8 +1,14 @@
 echo "Dieses Skript erstllelt ein Schlüsselpaar und öffnet die Ports 80 und 22"
-echo "Wie soll das Schlüsselpaar heissen?"
-read 
-aws ec2 create-key-pair --key-name aws-gbs-cli --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/aws-gbs-cli.pem
 
+echo "Schritt 1: Schlüsselpaar erstellen"
+sleep 3
+echo "Wie soll das Schlüsselpaar heissen? hier eingeben: "
+read KeyPairName
+aws ec2 create-key-pair --key-name $KeyPairName --key-type rsa --query 'KeyMaterial' --output text > ~/.ssh/aws-gbs-cli.pem
+
+echo "Schritt 2: Security Group erstellen"
+sleep 3
+echo "Wie soll die Security Group heissen? hier eingeben: "
 
 aws ec2 create-security-group --group-name gbs-sec-group --description "EC2-Webserver-SG"
 aws ec2 authorize-security-group-ingress --group-name gbs-sec-group --protocol tcp --port 80 --cidr 0.0.0.0/0
