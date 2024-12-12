75d30bd5b83e5dad13c6d606ef5d01546b3ec87b
Leandro Rutz <56978556+LNDRRTZ@users.noreply.github.com>
2024-12-12 10:55:00 +0100

    Update initskript.sh

--- initskript.sh
+++ initskript.sh
@@ -1,18 +1,23 @@
-# Variabeln importieren
+# Variablen importieren
 source Create_KeyPair.sh
 
-echo -n "Sind das die richtgen Werte?"
-echo -n "KeyPairName: $KeyPairName"
-echo -n "SecurityGroupName: $SecurityGroupName"
-echo -n "Falls Ja -> 1 | Falls Nein -> 0"
+echo -n "Sind das die richtigen Werte? "
+echo -n "KeyPairName: $KeyPairName "
+echo -n "SecurityGroupName: $SecurityGroupName "
+echo -n "Falls Ja -> 1 | Falls Nein -> 0 "
 echo -n "hier eingeben: "
 read Decision
 
-if [$Decision = 1]
+if [ "$Decision" = 1 ]; then
   echo "Super :) nun können wir weitermachen"
 else
   echo "Nicht gut... starte das Skript bitte erneut :("
+  exit 1
 fi
 
-# hier wird die die Ubuntu Umgebung erstellt
-aws ec2 run-instances --image-id ami-0e86e20dae9224db8 --count 1 --instance-type t2.micro --key-name $KeyPairName --security-groups $SecurityGroupName --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Webserver10}]'
+echo -n "Nun können wir den Namen der Ubuntu Maschine bestimmen"
+echo -n "Bitte hier eingeben: "
+read Name
+
+# hier wird die Ubuntu Umgebung erstellt
+aws ec2 run-instances --image-id ami-0e86e20dae9224db8 --count 1 --instance-type t2.micro --key-name "$KeyPairName" --security-groups "$SecurityGroupName" --iam-instance-profile Name=LabInstanceProfile --user-data file://initial.txt --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$Name}]"
